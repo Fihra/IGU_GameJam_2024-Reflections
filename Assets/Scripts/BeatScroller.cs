bed154086c7343faaf02aee0dfadead790b94b07
fihra <fabrofabian@gmail.com>
2024-08-24 00:42:26 -1000

    edit beatscroller for directions

--- Assets/Scripts/BeatScroller.cs
+++ Assets/Scripts/BeatScroller.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ScrollDirection
+{
+    TopScroll,
+    BottomScroll,
+    LeftScroll,
+    RightScroll
+        
+}
+
 public class BeatScroller : MonoBehaviour
 {
     public float tempo;
     public bool hasStarted;
+
+    public ScrollDirection direction;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +33,23 @@ public class BeatScroller : MonoBehaviour
         }
         else
         {
-            transform.position -= new Vector3(tempo * Time.deltaTime, transform.position.y, transform.position.z); 
+            switch(direction)
+            {
+                case ScrollDirection.LeftScroll:
+                    transform.position -= new Vector3(tempo * Time.deltaTime, transform.position.y, transform.position.z);
+                    break;
+                case ScrollDirection.RightScroll:
+                    transform.position += new Vector3(tempo * Time.deltaTime, 0, 0);
+                    break;
+                default:
+                    return;
+            }
+
+            //if(direction == ScrollDirection.LeftScroll)
+            //{
+            //    transform.position -= new Vector3(tempo * Time.deltaTime, transform.position.y, transform.position.z);
+            //}
+            
         }
     }
 }
